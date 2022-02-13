92d05939012491a47404d3cc70f13291af034fed
konmin123 <MindlinKS@yandex.ru>
2022-02-13 22:28:14 +0300

    a2_check_brackets end

--- Tasks/a3_check_brackets.py
+++ Tasks/a3_check_brackets.py
@@ -5,5 +5,17 @@ def check_brackets(brackets_row: str) -> bool:
     :param brackets_row: input string to be checked
     :return: True if valid, False otherwise
     """
-
-    return False
+    list_brackets = []
+    for brackets in brackets_row:
+        if brackets == "(":
+            list_brackets.append(brackets)
+        if brackets == ")":
+            if list_brackets:
+                list_brackets.remove("(")
+            else:
+                list_brackets.append("brackets_no_valid")
+                break
+    if list_brackets:
+        return False
+    else:
+        return True
