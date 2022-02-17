303be42ab13b7d89265efd64408caa8b00ec9c8b
konmin123 <MindlinKS@yandex.ru>
2022-02-17 19:12:53 +0300

    b1 end

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -1,25 +1,25 @@
 from typing import Sequence, Optional
 
 
-# def binary_search(elem: int, arr: Sequence) -> Optional[int]:
-#     """
-#     Performs binary search of given element inside of array
-#
-#     :param elem: element to be found
-#     :param arr: array where element is to be found
-#     :return: Index of element if it's presented in the arr, None otherwise
-#     """
-#     left = 0
-#     right = len(arr) - 1
-#     while left <= right:
-#         mid = (left + right) // 2
-#         if elem == arr[mid]:
-#             return mid
-#         elif arr[mid] < elem:
-#             left = mid + 1
-#         else:
-#             right = mid - 1
-#     print(elem, arr)
-#     return None
+def binary_search(elem: int, arr: Sequence) -> Optional[int]:
+    """
+    Performs binary search of given element inside of array
+
+    :param elem: element to be found
+    :param arr: array where element is to be found
+    :return: Index of element if it's presented in the arr, None otherwise
+    """
+    left = 0
+    right = len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if elem == arr[mid]:
+            return mid
+        elif arr[mid] < elem:
+            left = mid + 1
+        else:
+            right = mid - 1
+    print(elem, arr)
+    return None
 
 
