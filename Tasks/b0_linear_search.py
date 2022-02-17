716a529abaca5b0b5e8775a9b723b4ff38151a2c
konmin123 <MindlinKS@yandex.ru>
2022-02-17 18:48:42 +0300

    b0 end

--- Tasks/b0_linear_search.py
+++ Tasks/b0_linear_search.py
@@ -11,5 +11,11 @@ def min_search(arr: Sequence) -> int:
     :param arr: Array containing numbers
     :return: index of first occurrence of minimal element in array
     """
-    print(arr)
-    return -1
+
+    index_min_elem = 0
+    for i in range(len(arr)):
+        if arr[index_min_elem] > arr[i]:
+            index_min_elem = i
+    return index_min_elem
+
+
--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -1,13 +1,25 @@
 from typing import Sequence, Optional
 
 
-def binary_search(elem: int, arr: Sequence) -> Optional[int]:
-    """
-    Performs binary search of given element inside of array
+# def binary_search(elem: int, arr: Sequence) -> Optional[int]:
+#     """
+#     Performs binary search of given element inside of array
+#
+#     :param elem: element to be found
+#     :param arr: array where element is to be found
+#     :return: Index of element if it's presented in the arr, None otherwise
+#     """
+#     left = 0
+#     right = len(arr) - 1
+#     while left <= right:
+#         mid = (left + right) // 2
+#         if elem == arr[mid]:
+#             return mid
+#         elif arr[mid] < elem:
+#             left = mid + 1
+#         else:
+#             right = mid - 1
+#     print(elem, arr)
+#     return None
 
-    :param elem: element to be found
-    :param arr: array where element is to be found
-    :return: Index of element if it's presented in the arr, None otherwise
-    """
-    print(elem, arr)
-    return None
+
