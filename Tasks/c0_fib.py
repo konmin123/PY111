9edea1ba2883e2c08a340aa2f29eca3b0f20bba2
konmin123 <MindlinKS@yandex.ru>
2022-02-21 12:57:27 +0300

    123

--- Tasks/c0_fib.py
+++ Tasks/c0_fib.py
@@ -1,3 +1,5 @@
+from typing import Iterator
+
 def fib_recursive(n: int) -> int:
     """
     Calculate n-th number of Fibonacci sequence using recursive algorithm
@@ -16,5 +18,29 @@ def fib_iterative(n: int) -> int:
     :param n: number of item
     :return: Fibonacci number
     """
+    a = 0
+    b = 1
+    if n == 1:
+        return a
+    if n == 2:
+        return b
+    for _ in range(2, n):
+        a, b = b, a + b
+    return b
+
     print(n)
-    return 0
+    return b
+
+print(fib_iterative(10))
+
+def fib_generator(n: int) -> Iterator[int]:
+    a = 0
+    b = 1
+    yield a
+    yield b
+
+    for _ in range(n-2):
+        a, b = a, a + b
+        yield b
+
+print(fib_generator(10))
--- Tasks/c2_recursive_binary_search.py
+++ Tasks/c2_recursive_binary_search.py
@@ -1,13 +1,33 @@
 from typing import Sequence, Optional
 
 
-def binary_search(elem: int, arr: Sequence) -> Optional[int]:
+def binary_search(elem: int, arr: Sequence, left=None, right=None) -> Optional[int]:
     """
     Performs binary search of given element inside of array (using recursive way)
 
+    :param right:
+    :param left:
     :param elem: element to be found
     :param arr: array where element is to be found
     :return: Index of element if it's presented in the arr, None otherwise
     """
-    print(elem, arr)
-    return None
+
+    if left is None:
+        left = 0
+    if right is None:
+        right = len(arr) - 1
+    mid = left + (right - left) // 2
+    print(left, right, mid)
+
+    if left > right:
+        return None
+    if arr[mid] == elem:
+        return mid
+    elif elem < arr[mid]:
+        binary_search(elem, arr, left, mid - 1)
+    else:
+        binary_search(elem, arr, mid + 1, right)
+
+
+l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(binary_search(10, l))
